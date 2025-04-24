579eb897e87c887278fe7a924a572919bf31f259
Kyle <kylecng@gmail.com>
2025-04-24 16:40:58 -0400

    prettify initial monorepo

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,11 +4,11 @@ module.exports = {
   useTabs: false, // Indent lines with tabs instead of spaces.
   semi: true, // Print semicolons at the ends of statements.
   singleQuote: true, // Use single quotes instead of double quotes.
-  quoteProps: "as-needed", // Change when properties in objects are quoted.
+  quoteProps: 'as-needed', // Change when properties in objects are quoted.
   jsxSingleQuote: false, // Use single quotes instead of double quotes in JSX.
-  trailingComma: "es5", // Print trailing commas wherever possible in multi-line comma-separated syntactic structures. (e.g. arrays, objects)
+  trailingComma: 'es5', // Print trailing commas wherever possible in multi-line comma-separated syntactic structures. (e.g. arrays, objects)
   bracketSpacing: true, // Print spaces between brackets in object literals.
   bracketSameLine: false, // Put the `>` of a multi-line HTML (HTML, JSX, Vue, Angular) element at the end of the last line instead of being alone on the next line.
-  arrowParens: "always", // Include parentheses around a sole arrow function parameter.
-  endOfLine: "lf", // Ensure consistent line endings (Linux/macOS style).
+  arrowParens: 'always', // Include parentheses around a sole arrow function parameter.
+  endOfLine: 'lf', // Ensure consistent line endings (Linux/macOS style).
 };
